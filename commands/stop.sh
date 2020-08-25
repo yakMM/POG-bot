4fb156e9e26ee8c91be4b8a7f414ea74fb8340d4
Yak <yak.mayam@gmail.com>
2020-08-25 13:01:49 +0200

    Bot start scripts

--- commands/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-pkill -f main.py
-rm -f running
-echo "Bot stopped..."
--- commands/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-./stop.sh
-git reset --hard
-git clean -xdf
-git pull
-
-chmod a+x *
