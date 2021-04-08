48cf02d7253836d5f58eab1a9a50cf6d2bb7e344
YakMM <yak.mayam@gmail.com>
2021-04-08 19:16:45 +0200

    Added documentation to accounts_handler.py

--- bot/modules/accounts_handler.py
+++ bot/modules/accounts_handler.py
@@ -1,112 +1,165 @@
-import modules.config as cfg
+"""
+This module handle the POG Jaeger accounts
+"""
 
+# External imports
 from logging import getLogger
 from gspread import service_account
-from gspread.exceptions import APIError
-from numpy import array, vstack
-from modules.tools import UnexpectedError
-from display import AllStrings as disp, ContextWrapper
-
-from datetime import datetime as dt
-
+from numpy import array
 import discord.errors
 
+# Internal imports
+import classes
+from display import AllStrings as disp, ContextWrapper
 import modules.database as db
 import modules.reactions as reactions
+import modules.config as cfg
+from modules.tools import UnexpectedError
 
-import classes
 
 log = getLogger("pog_bot")
 
+# Will hold the Jaeger accounts
 _busy_accounts = dict()
 _available_accounts = dict()
+
+# Will handle account acceptance reactions
 _reaction_handler = reactions.ReactionHandler(rem_user_react=False, rem_bot_react=True)
 
+# Offsets in the google sheet
 X_OFFSET = 1
 Y_OFFSET = 2
 
 
+# Reaction handler callback on validation reaction
 @_reaction_handler.reaction('✅')
 async def on_account_reaction(reaction, player, user, msg):
+    # Get account, validate it and update the account message
     account = player.active.account
     await account.validate()
     await disp.ACC_UPDATE.edit(account.message, account=account)
 
 
-def init(secret_file):
+# Will be called at bot init or on account reload
+def init(secret_file: str):
+    """
+    Initialize the accounts from the google sheet.
+    If called later, reload the account usernames and passwords.
+
+    :param secret_file: Name of the gspread authentication json file.
+    """
+    # Open the google sheet:
     gc = service_account(filename=secret_file)
     sh = gc.open_by_key(cfg.database["accounts"])
     raw_sheet = sh.worksheet("1")
     sheet_tab = array(raw_sheet.get_all_values())
 
+    # Get total number of accounts
     num_accounts = sheet_tab.shape[0] - Y_OFFSET
 
-    # Get all accounts
+    # Add accounts one by one
     for i in range(num_accounts):
+        # Get account data
         a_id_str = sheet_tab[i + Y_OFFSET][X_OFFSET]
         a_username = sheet_tab[i + Y_OFFSET][X_OFFSET + 1]
         a_password = sheet_tab[i + Y_OFFSET][X_OFFSET + 2]
         a_id = int(a_id_str)
 
+        # Update account
         if a_id in _available_accounts:
             _available_accounts[a_id].update(a_username, a_password)
         elif a_id in _busy_accounts:
             _busy_accounts[a_id].update(a_username, a_password)
         else:
+            # If account doesn't exist already, initialize it
             unique_usages = db.get_field("accounts_usage", a_id, "unique_usages")
             if unique_usages is None:
                 raise UnexpectedError(f"Can't find usage for account {a_id}")
             _available_accounts[a_id] = classes.Account(a_id_str, a_username, a_password, unique_usages)
 
 
-async def give_account(a_player):
+async def give_account(a_player: classes.ActivePlayer) -> bool:
+    """
+    Give an account to a_player. We want each player to use as little accounts as possible.
+    So we try to give an account the player already used.
+
+    :param a_player: Player to give account to.
+    :return: True is account given, False if not enough accounts available.
+    """
+    # Get player usages
     unique_usages = await db.async_db_call(db.get_field, "accounts_usage", a_player.id, "unique_usages")
     if not unique_usages:
         unique_usages = list()
 
+    # Set player usages in the player object
     a_player.unique_usages = unique_usages
 
+    # If no available accounts, quit
     if len(_available_accounts) == 0:
         return False
 
+    # STEP 1.A: Check for accounts the player already used by the past
     potential = list()
     for acc_id in unique_usages:
         if acc_id in _available_accounts:
             potential.append(_available_accounts[acc_id])
 
+    # STEP 1.B: Give the account with the biggest usages amongst the accounts found in 1.A
     if potential:
         max_obj = potential[0]
         max_value = max_obj.nb_unique_usages
+        # Find max
         for acc in potential:
             n_usages = acc.nb_unique_usages
             if n_usages > max_value:
                 max_obj = acc
                 max_value = n_usages
+        # Give account, return ok
         _set_account(max_obj, a_player)
         return True
 
+    # STEP 2: If we couldn't find an account the player already used, give him the account with the least usages
     min_obj = list(_available_accounts.values())[0]
     min_value = min_obj.nb_unique_usages
+    # Find min
     for acc in _available_accounts.values():
         n_usages = acc.nb_unique_usages
         if n_usages < min_value:
             min_obj = acc
             min_value = n_usages
+    # Give account, return ok
     _set_account(min_obj, a_player)
     return True
 
 
-def _set_account(acc, a_player):
-    print(f"give {acc.id} to {a_player.name}")
+def _set_account(acc: classes.Account, a_player: classes.ActivePlayer):
+    """
+    Set player's account.
+
+    :param acc: Account to give.
+    :param a_player: Player who will receive the account.
+    """
+    log.info(f"Give account [{acc.id}] to player: id:[{a_player.id}], name:[{a_player.name}]")
+
+    # Put account in the busy dictionary
     del _available_accounts[acc.id]
     _busy_accounts[acc.id] = acc
+
+    # Set account
     acc.a_player = a_player
     acc.add_usage(a_player.id, a_player.match.id)
     a_player.account = acc
 
 
-async def send_account(channel, a_player):
+async def send_account(channel: discord.TextChannel, a_player: classes.ActivePlayer):
+    """
+    Actually send its account to the player.
+
+    :param channel: Current match channel.
+    :param a_player: Player to send the account to.
+    """
     msg = None
+    # Try 3 times to send a DM:
     for j in range(3):
         try:
             msg = await disp.ACC_UPDATE.send(ContextWrapper.user(a_player.id), account=a_player.account)
@@ -114,53 +167,75 @@ async def send_account(channel, a_player):
         except discord.errors.Forbidden:
             pass
     if msg:
+        # If DM ok, add the reaction handler for the validation
         reactions.add_handler(msg.id, _reaction_handler)
         await _reaction_handler.auto_add_reactions(msg)
-        a_player.account.message = msg
     else:
+        # Else validate the account and send it to staff channel instead
         await disp.ACC_CLOSED.send(channel, a_player.mention)
         await a_player.account.validate()
         msg = await disp.ACC_STAFF.send(ContextWrapper.channel(cfg.channels["staff"]),
                                         f'<@&{cfg.roles["admin"]}>', a_player.mention, account=a_player.account)
-        a_player.account.message = msg
+    # Set the account message, log the account:
+    a_player.account.message = msg
     await disp.ACC_LOG.send(ContextWrapper.channel(cfg.channels["spam"]), a_player.name, a_player.id, a_player.account.id)
 
 
-async def terminate_account(a_player):
+async def terminate_account(a_player: classes.ActivePlayer):
+    """
+    Terminate the account: ask the user to log off and remove the reaction.
+
+    :param a_player: Player whose account should be terminated.
+    """
+    # Get account and terminate it
     acc = a_player.account
     acc.terminate()
+
+    # Remove the reaction handler and update the account message
     reactions.rem_handler(acc.message.id)
     await disp.ACC_UPDATE.edit(acc.message, account=acc)
+
+    # If account was validated, ask the player to log off:
     if acc.is_validated and acc.message.channel.id != cfg.channels["staff"]:
         await disp.ACC_OVER.send(ContextWrapper.user(acc.a_player.id))
     else:
         await _reaction_handler.auto_remove_reactions(acc.message)
 
+    # If account was validated, update the db with usage
     if acc.is_validated:
-        # Update db
-        p_usage = dict()
-        p_usage["id"] = acc.id
-        p_usage["time_start"] = acc.last_usage["time_start"]
-        p_usage["time_stop"] = acc.last_usage["time_stop"]
-        p_usage["match_id"] = a_player.match.id
-        # Account entry
+        # Prepare data
+        p_usage = {
+            "id": acc.id,
+            "time_start": acc.last_usage["time_start"],
+            "time_stop": acc.last_usage["time_stop"],
+            "match_id": a_player.match.id
+        }
+        # Update the account element
         await db.async_db_call(db.push_element, "accounts_usage", acc.id, {"usages": acc.last_usage})
         try:
-            # Player entry
+            # Update the player element
             await db.async_db_call(db.push_element, "accounts_usage", a_player.id, {"usages": p_usage})
         except db.DatabaseError:
+            # If the player element doesn't exist, create it
             data = dict()
             data["_id"] = a_player.id
             data["unique_usages"] = a_player.unique_usages
             data["usages"] = [p_usage]
             await db.async_db_call(db.set_element, "accounts_usage", a_player.id, data)
 
+    # Reset the account state
     acc.clean()
     del _busy_accounts[acc.id]
     _available_accounts[acc.id] = acc
 
 
-def get_not_validated_accounts(team):
+def get_not_validated_accounts(team: classes.Team) -> list:
+    """
+    Find all the accounts that were not validated within the team.
+
+    :param team: Team to check
+    :return: List containing the players who didn't accept their accounts.
+    """
     not_ready = list()
     for p in team.players:
         if p.has_own_account:
