042850758a3984eb71f847fc820b10ca62148ba8
YakMM <yak.mayam@gmail.com>
2021-04-16 11:31:36 +0200

    weapon script comments

--- bot/weapons_script.py
+++ bot/weapons_script.py
@@ -1,39 +1,48 @@
-# @CHECK 2.0 features OK
+"""
+Script for populating the weapon database
+Tools for interacting with weapons listed in the census API
+"""
 
-import modules.config as cfg
-from modules.database import force_update, init as db_init, get_all_elements
+# External imports
 import requests
 import json
 import os
+
+# Internal imports
+import modules.config as cfg
+from modules.database import force_update, init as db_init, get_all_elements
 import classes
 
+# Find if we are in test mode or in production
 if os.path.isfile("test"):
     LAUNCHSTR = "_test"
 else:
     LAUNCHSTR = ""
 
+# Init config file and database, get all weapons currently in db
 cfg.get_config(LAUNCHSTR)
 db_init(cfg.database)
 get_all_elements(classes.Weapon, "static_weapons")
 
 item_type_id = 26  # weapon
 
+# List of all categories
 we_cats = {
     2: 'Knife',  # DET
     3: 'Pistol',  # DET
-    8: 'Carbine',  # ALL
-    7: 'Assault Rifle',  # ALL
-    139: 'Infantry Abilities',  # REMOVE
+    8: 'Carbine',  # DET
+    7: 'Assault Rifle',  # DET
+    139: 'Infantry Abilities',  # BAN
     4: 'Shotgun',  # BAN
-    6: 'LMG',  # ALL
+    6: 'LMG',  # DET
     13: 'Rocket Launcher',  # NP
     11: 'Sniper Rifle',  # DET
     18: 'Explosive',  # BAN
-    17: 'Grenade',  # NP
+    17: 'Grenade',  # DET
     5: 'SMG',  # DET
-    19: 'Battle Rifle',  # BAN
+    19: 'Battle Rifle',  # DET
     24: 'Crossbow',  # ALL
-    12: 'Scout Rifle',  # DET
+    12: 'Scout Rifle',  # ALL
     10: 'AI MAX (Left)',  # BAN
     14: 'Heavy Weapon',  # BAN
     21: 'AV MAX (Right)',  # BAN
@@ -42,24 +51,36 @@ we_cats = {
     9: 'AV MAX (Left)',  # BAN
     23: 'AA MAX (Left)',  # BAN
     147: 'Aerial Combat Weapon',  # BAN
-    104: 'Vehicle Weapons',  # REMOVE
-    211: 'Colossus Primary Weapon',  # REMOVE
-    144: 'ANT Top Turret',  # REMOVE
-    157: 'Hybrid Rifle',  # REMOVE
-    126: 'Reaver Wing Mount',  # REMOVE
-    208: 'Bastion Point Defense',  # REMOVE
-    209: 'Bastion Bombard',  # REMOVE
-    210: 'Bastion Weapon System'  # REMOVE
+    104: 'Vehicle Weapons',  # BAN
+    211: 'Colossus Primary Weapon',  # BAN
+    144: 'ANT Top Turret',  # BAN
+    157: 'Hybrid Rifle',  # BAN
+    126: 'Reaver Wing Mount',  # BAN
+    208: 'Bastion Point Defense',  # BAN
+    209: 'Bastion Bombard',  # BAN
+    210: 'Bastion Weapon System'  # BAN
 }
 
+# Discrimination per category
 ignored_categories = []  # [104,211,144,157,126,208,209,210,139] Switched this to banned
 banned_categories = [21, 20, 22, 9, 23, 10, 18, 147, 14, 4, 104, 211, 144, 157, 126, 208, 209, 210, 139]
 allowed_categories = [24, 12]
-detailled = [2, 3, 5, 6, 7, 8, 11, 17, 19]
+detailed = [2, 3, 5, 6, 7, 8, 11, 17, 19]
 no_point = [13, 17]
 
 
-def get_banned_per_categorie(cat, id):
+def get_banned_per_category(cat: int, w_id: int) -> bool:
+    """
+    Determine if a weapon is banned.
+
+    :param cat: Category of the weapon.
+    :param w_id: Id of the weapon.
+    :return: True if the weapon is banned, False if not.
+    """
+    d = dict()
+    # Edit this function to change the ruleset:
+    # To ban a weapon, add it to the dict, to unban it remove it.
+
     # Knife
     if cat == 2:
         d = {
@@ -151,7 +172,6 @@ def get_banned_per_categorie(cat, id):
             802771: "NS-AM7 Archer",
             802910: "NS-AM7B Archer",
             802921: "NS-AM7G Archer",
-            # 804255: "NSX Daimyo",
             6002918: "NS-AM7 VS/AE Archer",
             6002930: "NS-AM7 AE/TR Archer",
             6002943: "NS-AM7 AE/NC Archer",
@@ -159,8 +179,6 @@ def get_banned_per_categorie(cat, id):
             6004992: "NS-AM8 Shortbow",
             6006850: "SR-200",
             6008496: "PSA-01 Hammerhead AMR"
-            # 6008652: 'NSX "Ivory" Daimyo',
-            # 6008670: 'NSX "Networked" Daimyo'
         }
     # Grenades
     elif cat == 17:
@@ -190,32 +208,7 @@ def get_banned_per_categorie(cat, id):
             6005970: "NSX-A Sesshin",
             6009101: "NS-30 Tranquility"
         }
-    return id in d.keys()
-
-
-def get_weapons_categories():
-    url = f'http://census.daybreakgames.com/s:{cfg.general["api_key"]}/get/ps2:v2/item/?item_type_id=26&is_vehicle_weapon=0&c:limit=5000&c:show=item_id,item_category_id,name.en'
-    response = requests.get(url)
-    jdata = json.loads(response.content)
-    if jdata["returned"] == 0:
-        print("Error")
-        return
-    cats = list()
-
-    for we in jdata["item_list"]:
-        we["_id"] = int(we.pop("item_id"))
-        try:
-            we["name"] = we.pop("name")["en"]
-        except KeyError:
-            print(f'Key on name of id {we["_id"]}')
-        try:
-            we["cat_id"] = int(we.pop("item_category_id"))
-            if we["cat_id"] not in cats:
-                cats.append(we["cat_id"])
-        except KeyError:
-            print(f'Key on cat of id {we["_id"]}')
-
-    return cats
+    return w_id in d.keys()
 
 
 def get_unknown_weapon():
@@ -229,90 +222,140 @@ def get_unknown_weapon():
     return n_data
 
 
-def push_all_weapons(find_new=False):
+def push_all_weapons(push_db=False):
     giga_list = list()
     for cat in we_cats.keys():
+
+        # If category to ignore
         if cat in ignored_categories:
             continue
-        url = f'http://census.daybreakgames.com/s:{cfg.general["api_key"]}/get/ps2:v2/item/?item_type_id=26&is_vehicle_weapon=0&item_category_id={cat}&c:limit=5000&c:show=item_id,item_category_id,name.en,faction_id'
+
+        # Else get all weapons from the category
+        url = f'http://census.daybreakgames.com/s:{cfg.general["api_key"]}/get/ps2:v2/item/' \
+              f'?item_type_id=26&is_vehicle_weapon=0&item_category_id={cat}' \
+              f'&c:limit=5000&c:show=item_id,item_category_id,name.en,faction_id'
         response = requests.get(url)
-        jdata = json.loads(response.content)
-        print(we_cats[cat])
-        if jdata["returned"] == 0:
-            print("Error")
-            return
-        for we in jdata["item_list"]:
+        j_data = json.loads(response.content)
+        print(we_cats[cat])  # Print category name
+
+        if j_data["returned"] == 0:
+            raise ValueError("Nothing returned!")
+
+        # Iterate trough weapons of this category
+        for we in j_data["item_list"]:
             n_data = dict()
-            n_data["_id"] = int(we["item_id"])
+            n_data["_id"] = int(we["item_id"])  # Weapon ID
             if n_data["_id"] == 0:
-                print("RAAAAA")
+                raise ValueError("ID = 0")
             try:
-                n_data["name"] = we["name"]["en"]
+                n_data["name"] = we["name"]["en"]  # Weapon name
             except KeyError:
                 n_data["name"] = "Unknown"
                 print("Unknown weapon, id: " + we["item_id"])
-            n_data["cat_id"] = int(we["item_category_id"])
+            try:
+                n_data["faction"] = int(we["faction_id"])  # Get weapon's faction
+            except KeyError:
+                n_data["faction"] = 0
+            n_data["cat_id"] = int(we["item_category_id"])  # Weapon category
+
+            # If whole category is banned, weapon is banned
             if cat in banned_categories:
                 n_data["points"] = 0
                 n_data["banned"] = True
-            elif cat in detailled:
-                if get_banned_per_categorie(cat, n_data["_id"]):
+            elif cat in detailed:
+                # Else find if weapon is banned
+                if get_banned_per_category(cat, n_data["_id"]):
+                    # Banned
                     n_data["points"] = 0
                     n_data["banned"] = True
                 else:
+                    # Not banned, check if weapon should give points
                     if cat in no_point:
                         n_data["points"] = 0
                     else:
                         n_data["points"] = 1
                     n_data["banned"] = False
+
+            # If whole category is allowed
             elif cat in allowed_categories:
                 n_data["points"] = 1
                 n_data["banned"] = False
+
+            # If whole category gives no point
             elif cat in no_point:
                 n_data["points"] = 0
                 n_data["banned"] = False
-            try:
-                n_data["faction"] = int(we["faction_id"])
-            except KeyError:
-                n_data["faction"] = 0
+
             giga_list.append(n_data)
-            # To find new weapons:
-            if find_new:
-                if not classes.Weapon.get(n_data["_id"]):
-                    print(
-                        f'Weapon not found in database: cat : {n_data["cat_id"]}, name: {n_data["name"]}, id: {n_data["_id"]}')
+
+            # Find weapons new to database:
+            if not classes.Weapon.get(n_data["_id"]):
+                print(f'Weapon not found in database: '
+                      f'cat : {n_data["cat_id"]}, name: {n_data["name"]}, id: {n_data["_id"]}')
+
+    # Add the unknown weapon to the list
     giga_list.append(get_unknown_weapon())
-    if not find_new:
-        print("DB updated!")
+    if push_db:
+        # Update db
         force_update("static_weapons", giga_list)
+        print("DB updated!")
     else:
-        print("DB not updated! Use arg find_new=False to update")
+        print("DB not updated! Use arg 'push_db=True' to update")
 
 
-def displayWeaponsFromCategorie(cat):
-    url = f'http://census.daybreakgames.com/s:{cfg.general["api_key"]}/get/ps2:v2/item/?item_type_id=26&is_vehicle_weapon=0&item_category_id={cat}&c:limit=5000&c:show=item_id,item_category_id,name.en,faction_id'
+push_all_weapons(True)
+
+
+def display_weapons_from_category(cat):
+    url = f'http://census.daybreakgames.com/s:{cfg.general["api_key"]}/get/ps2:v2/item/' \
+          f'?item_type_id=26&is_vehicle_weapon=0&item_category_id={cat}' \
+          f'&c:limit=5000&c:show=item_id,item_category_id,name.en,faction_id'
     response = requests.get(url)
-    jdata = json.loads(response.content)
-    if jdata["returned"] == 0:
+    j_data = json.loads(response.content)
+    if j_data["returned"] == 0:
         print("Error")
         return
-    for we in jdata["item_list"]:
+    for we in j_data["item_list"]:
         print(f"{we['item_id']};{we['name']['en']}")
 
 
 def get_all_categories():
     url = f'http://census.daybreakgames.com/s:{cfg.general["api_key"]}/get/ps2:v2/item_category/?c:limit=500'
     response = requests.get(url)
-    jdata = json.loads(response.content)
+    j_data = json.loads(response.content)
 
-    if jdata["returned"] == 0:
+    if j_data["returned"] == 0:
         print("Error")
         return
 
     di = dict()
-    for cat in jdata["item_category_list"]:
+    for cat in j_data["item_category_list"]:
         di[int(cat["item_category_id"])] = cat["name"]["en"]
     return di
 
 
-push_all_weapons()
+def get_weapons_categories():
+    url = f'http://census.daybreakgames.com/s:{cfg.general["api_key"]}/get/ps2:v2/item/' \
+          f'?item_type_id=26&is_vehicle_weapon=0&c:limit=5000' \
+          f'&c:show=item_id,item_category_id,name.en'
+    response = requests.get(url)
+    j_data = json.loads(response.content)
+    if j_data["returned"] == 0:
+        print("Error")
+        return
+    cats = list()
+
+    for we in j_data["item_list"]:
+        we["_id"] = int(we.pop("item_id"))
+        try:
+            we["name"] = we.pop("name")["en"]
+        except KeyError:
+            print(f'KeyError on name of id {we["_id"]}')
+        try:
+            we["cat_id"] = int(we.pop("item_category_id"))
+            if we["cat_id"] not in cats:
+                cats.append(we["cat_id"])
+        except KeyError:
+            print(f'KeyError on cat of id {we["_id"]}')
+
+    return cats
