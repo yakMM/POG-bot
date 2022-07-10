db07a08b4029fc909f132573cd70a0379c26a202
YakMM <yak.mayam@gmail.com>
2022-07-10 12:19:32 +0200

    Fix for timeout sending the same message twice when used in timeout channel

--- bot/cogs/admin.py
+++ bot/cogs/admin.py
@@ -223,7 +223,8 @@ class AdminCog(commands.Cog, name='admin'):
         await roles.role_update(player)
         await player.db_update("timeout")
         await roles.perms_muted(True, player.id)
-        await disp.RM_TIMEOUT.send(ctx, player.mention, dt.utcfromtimestamp(end_time).strftime("%Y-%m-%d %H:%M UTC"))
+        if ctx.channel.id != cfg.channels['muted']:
+            await disp.RM_TIMEOUT.send(ctx, player.mention, dt.utcfromtimestamp(end_time).strftime("%Y-%m-%d %H:%M UTC"))
         await disp.RM_TIMEOUT.send(ContextWrapper.channel(cfg.channels['muted']), player.mention, dt.utcfromtimestamp(end_time).strftime("%Y-%m-%d %H:%M UTC"))
 
     @commands.command()
