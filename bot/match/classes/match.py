affb5d979b54c0d6f6cd93996534ef20a9c3da54
Varunda <66178574+Varunda@users.noreply.github.com>
2024-04-08 15:45:48 -0700

    Update bot/match/classes/match.py
    Co-authored-by: Yak <68242321+yakMM@users.noreply.github.com>

--- bot/match/classes/match.py
+++ bot/match/classes/match.py
@@ -134,7 +134,7 @@ class Match:
     def spin_up(self, p_list):
         if not self.__objects:
             raise AttributeError("Match instance is not bound, no attribute 'spin_up'")
-        if (Match._last_match_id == None): # if there was no last match, make sure it exists
+        if Match._last_match_id is None: # if there was no last match, make sure it exists
             Match._last_match_id = 0
             db.set_element("restart_data", 0, {"_id": 0, "last_match_id": Match._last_match_id})
         Match._last_match_id += 1
